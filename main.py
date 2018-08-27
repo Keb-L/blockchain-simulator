c02b5f16095fb2cb4898d7085ccda8fa8d45b8f4
vishaalagartha <vishaalagartha@gmail.com>
2018-08-27 10:45:14 -0700

    adds params.json to input parameters. closes #1

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-import sys, os, shutil
+import sys, os, shutil, json
 from optparse import OptionParser
 from coordinator import Coordinator
 from node import Node
 import generate_dataset
 
+def get_params(filename):
+    params = {}
+    with open(f'{filename}') as f:
+        d = json.load(f)
+        params['num_nodes'] = d['Number of nodes'] 
+        params['max_block_size'] = d['Block size (txs)']
+        params['tx_error_prob'] = d['Probability of error in transaction confirmation']
+        params['proposal_rate'] = d['Block proposal rate parameter']
+        params['dataset'] = d['Transaction dataset']
+        params['model'] = d['Network model']
+        params['duration'] = d['Duration (sec)']
+    return params
+
 if __name__=='__main__':
     usage = 'usage: python %prog [options]'
     parser = OptionParser(usage=usage)
 
-    parser.add_option('-n', '--num_nodes', type='int',
-            action='store', dest='num_nodes', default=1,
-            help='number of nodes in network; default is 1')
-
-    parser.add_option('-b', '--block_size', type='int',
-            action='store', dest='max_block_size', default=3,
-            help='maximum number of transactions in a block; default is 3')
-
-    parser.add_option('-e', '--epsilon', type='float',
-            action='store', dest='epsilon', default=0.0,
-            help='probability of error in transaction confirmation; default is 0.0')
-
-    parser.add_option('-f', '--block_proposal_rate', type='float',
-            action='store', dest='block_proposal_rate', default=0.5,
-            help='rate of block proposals; default is 0.5')
-
-    parser.add_option('-T', '--duration', type='int',
-            action='store', dest='duration', default=5,
-            help='duration of experiment is seconds; default is 5 seconds')
-
+    parser.add_option('-f', '--filename', type='string',
+            action='store', dest='filename', default='params.json',
+            help='filename to set parameters; default parameters are in params.json')
 
     (options, args) = parser.parse_args(sys.argv[1:])
+
+    params = get_params(options.filename)
 
     # Setup logging directory 
     shutil.rmtree('./logs')
@@ -38,7 +36,7 @@ if __name__=='__main__':
     c = Coordinator() 
     nodes = []
     # generate num_nodes nodes
-    for node_id in range(0, options.num_nodes): 
+    for node_id in range(0, params['num_nodes']): 
         n = Node(node_id)
         nodes.append(n)
         c.add_node(n)
@@ -50,13 +48,13 @@ if __name__=='__main__':
                 nodes[i].add_neighbor(nodes[j])
     
     # generate mock poisson and deterministic dataset
-    poisson_dataset = generate_dataset.poisson(0.1, options.duration, c.clock, c.nodes)
-    deterministic_dataset = generate_dataset.deterministic(0.1, options.duration, c.clock, c.nodes)
+    poisson_dataset = generate_dataset.poisson(0.1, params['duration'], c.clock, c.nodes)
+    deterministic_dataset = generate_dataset.deterministic(0.1, params['duration'], c.clock, c.nodes)
 
     # generate proposal events
-    c.generate_proposals(options.block_proposal_rate, options.duration)
+    c.generate_proposals(params['proposal_rate'], params['duration'])
     # set transaction dataset
     c.set_transactions(deterministic_dataset)
 
     # run simulation
-    c.run(options.max_block_size)
+    c.run(params['max_block_size'])
