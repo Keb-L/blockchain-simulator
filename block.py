91717ada631b4bca8f3e02dbc8e2f8d056b75743
vishaalagartha <vishaalagartha@gmail.com>
2018-08-29 12:06:34 -0700

    Shorter ids for blocks and transactions

--- block.py
+++ block.py
@@ -3,7 +3,7 @@ import numpy as np, uuid
 class Block():
     def __init__(self):
         self.txs = np.array([])
-        self.id = uuid.uuid4().hex
+        self.id = uuid.uuid4().hex[0:3]
         self.children = set()
 
     def add_tx(self, tx):
