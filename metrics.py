02619dd45a6e200ae7c7552779423513c07e8e92
vishaalagartha <vishaalagartha@gmail.com>
2018-09-07 12:17:19 -0700

    Fixes metrics to separate latency into 2 separate concepts. closes #26

--- metrics.py
+++ metrics.py
@@ -1,4 +1,4 @@
-import re, json, pprint
+import re, json, pprint, csv
 from constants import TX_RATE
 
 def dump_params():
@@ -17,56 +17,27 @@ def compute_throughput():
     return TX_RATE
 
 def compute_latency():
-    parse_transaction = False
-    d = {}
-    avg_latency = 0.0
-    finalized_tx_count = 0
-    with open('logs/data.log') as f:
-        lines = f.readlines()
-        i = 0
-
-        # get all transaction generation times
-        # get all finalized blocks
-        prev_fin_index = 0
-        while i<len(lines):
-            line = lines[i]
-            if line=='Transactions:\n':
-                parse_transaction = True
-            elif line.startswith('Finalized blocks at'):
-                parse_transaction=False
-                prev_fin_index = i
-            elif parse_transaction:
-                l = line.split(',')
-                timestamp = float(l[0].split('time: ')[1])
-                id = l[1].split('id: ')[1]
-                d[id] = {'generation_timestamp': timestamp}
-            i+=1
-
-        # get finalization timestamp
-        finalization_timestamp = float(re.search('Finalized blocks at (.*):',
-                lines[prev_fin_index]).group(1))
-
-        # sum latencies up for all transactions
-        for line in lines[prev_fin_index+1:]:
-            finalized_txs = line.rstrip().split(':')[1].split(',')
-            for tx in finalized_txs:
-                if tx in d:
-                    latency = finalization_timestamp-d[tx]['generation_timestamp']
-                    d[tx] = {'generation_timestamp': d[tx]['generation_timestamp'],
-                            'finalization_timestamp': finalization_timestamp,
-                            'latency': latency}
-                    finalized_tx_count+=1
-                    avg_latency+=latency
-
-    if finalized_tx_count!=0:
-        avg_latency = avg_latency/finalized_tx_count
-
-    return avg_latency
+    with open('./logs/transactions.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        main_chain_arrival_sum = 0
+        main_chain_arrival_count = 0
+        finalization_sum = 0
+        finalization_count = 0
+        for row in reader:
+            if row['Main Chain Arrival Timestamp']!='None':
+                main_chain_arrival_sum += float(row['Main Chain Arrival Timestamp']) - float(row['Arrival Timestamp'])
+                main_chain_arrival_count += 1
+            if row['Finalization Timestamp']!='None':
+                finalization_sum += float(row['Finalization Timestamp']) - float(row['Main Chain Arrival Timestamp'])
+                finalization_count += 1
+    return main_chain_arrival_sum/main_chain_arrival_count, finalization_sum/finalization_count
 
 def dump_results():
     print('Results:')
+    avg_main_chain_arrival_latency, avg_finalization_latency = compute_latency() 
     print(f'Transaction Throughput: {compute_throughput()} transactions/sec')
-    print(f'Transaction Latency: {compute_latency()} sec/transaction')
+    print(f'Main Chain Arrival Latency: {avg_main_chain_arrival_latency} sec/transaction')
+    print(f'Finalization Latency: {avg_finalization_latency} sec/transaction')
 
 if __name__=='__main__':
     dump_params()
