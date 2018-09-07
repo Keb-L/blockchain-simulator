729a775767c5c1fe0494a670ac2c64809f7aa5a8
vishaalagartha <vishaalagartha@gmail.com>
2018-09-07 11:06:28 -0700

    Adds confirmation and arrival timestamps to events

--- events.py
+++ events.py
@@ -6,9 +6,26 @@ class Transaction():
         self.source = source
         self.id = uuid.uuid4().hex[0:10]
 
+        self.main_chain_timestamp = None
+        self.confirmation_timestamp = None
+
+    def set_main_chain_arrival(self, main_chain_timestamp):
+        self.main_chain_timestamp = main_chain_timestamp
+
+    def set_confirmation_timestamp(self, confirmation_timestamp):
+        self.confirmation_timestamp = confirmation_timestamp
+
 class Proposal():
     def __init__(self, timestamp):
         self.timestamp = timestamp
+        self.main_chain_timestamp = None
+        self.confirmation_timestamp = None
 
     def set_block(self, block):
         self.block = block
+
+    def set_main_chain_arrival(self, main_chain_timestamp):
+        self.main_chain_timestamp = main_chain_timestamp
+
+    def set_confirmation_timestamp(self, confirmation_timestamp):
+        self.confirmation_timestamp = confirmation_timestamp
