cf2219b4ad9eef40c4bc8adec0740a2108dd8f48
vishaalagartha <vishaalagartha@gmail.com>
2018-09-09 13:30:31 -0700

    Adds proposal and finalization timestamp to block class

--- block.py
+++ block.py
@@ -1,7 +1,7 @@
 import numpy as np, uuid
 
 class Block():
-    def __init__(self, txs=None, id=None, parent_id=None):
+    def __init__(self, txs=None, id=None, parent_id=None, proposal_timestamp):
         if txs is None:
             self.txs = np.array([])
         else:
@@ -12,9 +12,15 @@ class Block():
             self.id = id
 
         self.parent_id = parent_id 
+        self.proposal_timestamp = proposal_timestamp
+
+        self.finalization_timestamp = None
 
     def add_tx(self, tx):
         self.txs = np.append(self.txs, tx)
 
     def set_parent_id(self, parent_id):
         self.parent_id = parent_id
+
+    def set_finalization_timestamp(self, finalization_timestamp):
+        self.finalizatin_timestamp = finalization_timestamp
