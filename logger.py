681e7b1bd53d385cc31b383d13e8f1f9781b3188
vishaalagartha <vishaalagartha@gmail.com>
2018-09-10 11:53:11 -0700

    Adds ipython log loading file

--- logger.py
+++ logger.py
@@ -36,26 +36,23 @@ def log_txs(txs):
                 f'{tx.finalization_timestamp}'})
 
 def log_statistics(params, global_blocktree):
-    with open('./logs/stats.log', 'w+') as f:
+    with open('./logs/stats.csv', 'w+') as csvfile:
         # log network latency information
         if params['model']=='Decker-Wattenhorf' or params['model']=='Constant-Decker-Wattenhorf':
-            f.write(f'Average network latency for blocks: {constant_decker_wattenhorf(params["max_block_size"])} sec\n')
-            f.write(f'Average network latency for txs: {constant_decker_wattenhorf(TX_SIZE)} sec\n')
+            csvfile.write(f'Average network latency for blocks (sec),{constant_decker_wattenhorf(params["max_block_size"])}\n')
+            csvfile.write(f'Average network latency for txs (sec),{constant_decker_wattenhorf(TX_SIZE)}\n')
 
         # log main chain information blocks
         num_blocks = len(global_blocktree.tree.get_vertices())
         main_chain_length = len(global_blocktree.main_chain())
         num_orphan_blocks = num_blocks - main_chain_length 
-        f.write(f'Number of blocks: {num_blocks}\n')
-        f.write(f'Main chain length: {main_chain_length}\n')
-        f.write(f'Number of orphan blocks: {num_orphan_blocks}\n')
-        f.write(f'Fraction of orphan blocks: {float(num_orphan_blocks)/num_blocks}\n')
-
-
+        csvfile.write(f'Number of blocks,{num_blocks}\n')
+        csvfile.write(f'Main chain length,{main_chain_length}\n')
+        csvfile.write(f'Number of orphan blocks,{num_orphan_blocks}\n')
+        csvfile.write(f'Fraction of orphan blocks,{float(num_orphan_blocks)/num_blocks}\n')
 
 def draw_global_blocktree(global_blocktree):
     main_chain_vp = global_blocktree.tree.new_vertex_property('int')
-
 
     # color main chain a different color
     for v in global_blocktree.main_chain():
