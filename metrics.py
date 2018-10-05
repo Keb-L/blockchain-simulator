ad76ef11cc54db54d606cb5307bf71691f385652
vishaalagartha <vishaalagartha@gmail.com>
2018-10-05 09:55:38 -0700

    allows file option to metrics

--- metrics.py
+++ metrics.py
@@ -1,4 +1,4 @@
-import re, json, pprint, csv, glob
+import sys, re, json, pprint, csv, glob
 from constants import TX_RATE
 
 def dump_params():
@@ -12,26 +12,26 @@ def dump_params():
     pp.pprint(d)
     print('\n')
 
-def compute_throughputs():
+def compute_throughputs(foldername='logs'):
     with open(f'params.json') as f:
         contents = json.load(f)
         setting_name = contents['setting-name']
         d = contents[setting_name]
         duration = d['Duration (sec)']
-    with open('./logs/transactions.csv', newline='') as csvfile:
+    with open(f'{foldername}/transactions.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         num_transactions_finalized = 0
         num_unique_transactions_finalized = 0
         for row in reader:
             if row['finalization timestamps']!='None':
-                num_transactions_finalized+=1
-                if ';' not in row['finalization timestamps']:
-                    num_unique_transactions_finalized+=1
+                num_unique_transactions_finalized+=1
+                if ';' in row['finalization timestamps']:
+                    num_transactions_finalized+=len(row['finalization timestamps'].split(';'))
 
     return float(num_transactions_finalized)/duration, float(num_unique_transactions_finalized)/duration
 
-def compute_latency():
-    with open('./logs/transactions.csv', newline='') as csvfile:
+def compute_latency(foldername='logs'):
+    with open(f'{foldername}/transactions.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         pool_block_arr_sum = 0
         pool_block_arr_count = 0
@@ -63,10 +63,10 @@ def compute_latency():
     avg_finalization_latency = 0 if finalization_count==0 else finalization_sum/finalization_count
     return avg_pool_block_arr_latency, avg_pool_block_ref_latency, avg_main_chain_arrival_latency, avg_finalization_latency
 
-def dump_results():
+def dump_results(foldername='logs'):
     print('Results:')
-    avg_pool_block_arr_latency, avg_pool_block_ref_latency, avg_main_chain_arrival_latency, avg_finalization_latency = compute_latency() 
-    transaction_throughput, unique_transaction_throughput = compute_throughputs()
+    avg_pool_block_arr_latency, avg_pool_block_ref_latency, avg_main_chain_arrival_latency, avg_finalization_latency = compute_latency(foldername) 
+    transaction_throughput, unique_transaction_throughput = compute_throughputs(foldername)
     print(f'Transaction Throughput: {transaction_throughput} transactions/sec')
     print(f'Unique Transaction Throughput: {unique_transaction_throughput} transactions/sec')
     print(f'Pool Block Arrival Latency: {avg_pool_block_arr_latency} sec/transaction')
@@ -75,5 +75,10 @@ def dump_results():
     print(f'Finalization Latency: {avg_finalization_latency} sec/transaction')
 
 if __name__=='__main__':
+    print(sys.argv)
+    if len(sys.argv)>1:
+        foldername = sys.argv[1]
+    else:
+        foldername = 'logs'
     dump_params()
-    dump_results()
+    dump_results(foldername)
